7d00f89b331780617c3156e2b3d1b97582aa20ba
Agam <agam.rai0123@gmail.com>
2024-09-11 22:00:13 +0530

    trying mouse press

--- main.go
+++ main.go
@@ -14,32 +14,31 @@ func main() {
 func setup() {
 	p5.Canvas(600, 600)
 	p5.Background(color.Gray{Y: 220})
+
 }
 
 func draw() {
-	p5.StrokeWidth(4)
-	p5.Stroke(color.Black)
-	p5.Line(0, 200, 600, 200)
-	p5.Line(0, 400, 600, 400)
-	p5.Line(200, 0, 200, 600)
-	p5.Line(400, 0, 400, 600)
+	coord := [9]string{}
+	state := "x"
+	drawGrid()
+
 	switch {
 	case p5.Event.Mouse.Pressed:
-		if p5.Event.Mouse.Buttons.Contain(p5.ButtonLeft) {
-			p5.Stroke(color.Black)
-			p5.Fill(color.RGBA{R: 255, A: 255})
+		N := getGrid(p5.Event.Mouse.Position.X, p5.Event.Mouse.Position.Y)
+		p5.Text(strconv.Itoa(N), 300, 300)
+		x, y := getCoord(N)
+		p5.Text(strconv.Itoa(N), x, y)
+		coord[N-1] = state
+		if state == "x" {
+			state = "o"
+		} else {
+			state = "x"
 		}
 	default:
-		p5.Stroke(nil)
-		p5.Fill(color.Transparent)
 	}
-	p5.Ellipse(
-		p5.Event.Mouse.Position.X,
-		p5.Event.Mouse.Position.Y,
-		80, 80,
-	)
-	// fmt.Println(getGrid(p5.Event.Mouse.Position.X, p5.Event.Mouse.Position.Y))
-	p5.Text(strconv.Itoa(getGrid(p5.Event.Mouse.Position.X, p5.Event.Mouse.Position.Y)), 300, 300)
+
+	states(coord[:])
+	// Win(1, 9)
 }
 
 func getGrid(x float64, y float64) int {
@@ -51,4 +50,60 @@ func getGrid(x float64, y float64) int {
 		return 0
 	}
 	return row*3 + col + 1
+}
+
+func getCoord(n int) (float64, float64) {
+	gridSize := 200
+	row := (n - 1) / 3
+	col := (n - 1) % 3
+	return float64(col)*float64(gridSize) + float64(gridSize)/2, float64(row)*float64(gridSize) + float64(gridSize)/2
+}
+
+func drawCircle(x float64, y float64) {
+	p5.StrokeWidth(4)
+	p5.Stroke(color.Black)
+	p5.Circle(x, y, 100)
+}
+
+func drawCross(x float64, y float64) {
+	p5.StrokeWidth(4)
+	p5.Stroke(color.Black)
+	p5.Line(x-50, y-50, x+50, y+50)
+	p5.Line(x-50, y+50, x+50, y-50)
+}
+
+func drawGrid() {
+	p5.StrokeWidth(4)
+	p5.Stroke(color.Black)
+	p5.Line(0, 200, 600, 200)
+	p5.Line(0, 400, 600, 400)
+	p5.Line(200, 0, 200, 600)
+	p5.Line(400, 0, 400, 600)
+}
+
+func states(c []string) {
+	// for i := 1; i <= 9; i++ {
+	// 	x, y := getCoord(i)
+	// 	if i%2 == 0 {
+	// 		drawCircle(x, y)
+	// 	} else {
+	// 		drawCross(x, y)
+	// 	}
+	// }
+	for i := 0; i < 9; i++ {
+		if c[i] == "x" {
+			drawCross(getCoord(i + 1))
+		} else if c[i] == "o" {
+			drawCircle(getCoord(i + 1))
+		}
+	}
+}
+
+func Win(p1 int, p2 int) {
+	p5.StrokeWidth(30)
+	p5.Stroke(color.RGBA{R: 255, G: 0, B: 0, A: 255})
+	x1, y1 := getCoord(p1)
+	x2, y2 := getCoord(p2)
+
+	p5.Line(x1, y1, x2, y2)
 }
